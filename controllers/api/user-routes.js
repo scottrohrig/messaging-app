187bca5c770143a226833c1a1c7c11f59d420957
scottrohrig <scott.rohrig@gmail.com>
2022-02-07 15:15:52 -0800

    psuedo code api routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router;
+
+// GET all users
+
+// GET user by id
+
+// CREATE new user
+
+// Login
+
+// Logout
+
+// UPDATE password
+
+// DELETE user
+
+module.exports = router;
