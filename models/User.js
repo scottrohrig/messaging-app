648936dbea27b851c9a905bbc56cda99ada1613e
scottrohrig <scott.rohrig@gmail.com>
2022-02-09 15:02:24 -0800

    add User model columns and modularization

--- models/User.js
+++ models/User.js
@@ -1,13 +1,14 @@
 // require Model & DataTypes from sequelize
 const { Model, DataTypes } = require('sequelize');
+// require bcrypt
+const bcrypt = require('bcrypt');
 // require sequelize from connection.js
 const sequelize = require('../config/connection');
-// require bcrypt
-const bcrypt = requier('bcrypt');
 
 // define the User class that extends Model
 class User extends Model {
   // create an instance method that checks the password
+  // eslint-disable-next-line class-methods-use-this
   checkPassword(loginPw) {
     // return compared password from bcrypt. Consider making this asynchronous
   }
@@ -15,15 +16,49 @@ class User extends Model {
 
 // initialize the User Model's columns and options,
 User.init(
-  {},
   {
-    hooks: {
-      // and add a beforeCreate method to the hooks to encrypt the user password
-      async beforeCreate(newUserData) {
-        // hash the newUserData.password
-        // return the newUserData
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
       },
     },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 20],
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        // eslint-disable-next-line no-param-reassign
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
   }
 );
 
--- models/index.js
+++ models/index.js
@@ -1,9 +1,17 @@
 // import each Model
+const User = require('./User');
 
 // setup User associations
+// user has many conversations
+// user has many messages
 
 // setup Conversation associations
+// conversation has many users
+// conversation has many messages
 
 // setup message associations
+// message belongs to one conversation
+// message belongs to one user
 
 // modularize this file by exporting an object containing each Model
+module.exports = { User };
