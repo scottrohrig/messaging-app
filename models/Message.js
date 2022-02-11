04b904a9b91acb93c8a18f26827b78ab11e5a4c7
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-02-11 09:36:18 -0800

    modified models/Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,45 @@
 // require Model & DataTypes from sequelize
-
+const { Model, DataTypes } = require('sequelize');
 // require sequelize from connection.js
-
+const sequelize = require('../config/connection');
 // define the Message class that extends Model
-
+class Message extend Model {}
 
 // initialize the Message columns and options
-
+Message.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        message_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        conversation_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'conversation',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'message'
+    }
+);
 // modularize this script by exporting Message
+module.exports = Message;
