f59917bac7b76fc92b81fec584f30b55c182c3f4
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-02-11 10:49:11 -0800

    modified models/User.js

--- models/User.js
+++ models/User.js
@@ -1,63 +1,77 @@
 // require Model & DataTypes from sequelize
 const { Model, DataTypes } = require('sequelize');
-// require bcrypt
-const bcrypt = require('bcrypt');
 // require sequelize from connection.js
 const sequelize = require('../config/connection');
+// require bcrypt
+const bcrypt = requier('bcrypt');
 
 // define the User class that extends Model
 class User extends Model {
   // create an instance method that checks the password
-  // eslint-disable-next-line class-methods-use-this
   checkPassword(loginPw) {
     // return compared password from bcrypt. Consider making this asynchronous
+    return bcrypt.compareSync(loginPW, this.password);
   }
 }
 
 // initialize the User Model's columns and options,
 User.init(
   {
+    // define an id column
     id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
       type: DataTypes.INTEGER,
-      autoIncrement: true,
+      // equivalent of SQL "NOT NULL"
+      allowNull: false,
+      // instruct that this is the Primary Key
       primaryKey: true,
-      allowNull: false,
+      // turn on auto increment
+      autoIncrement: true,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      },
-    },
+    // define a username column
     username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      validate: {
-        len: [2, 20],
-      },
     },
+    // define a password column
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [4],
+        // this means the password must be at least eight characters long
+        len: [8],
       },
     },
   },
   {
     hooks: {
+      // and add a beforeCreate method to the hooks to encrypt the user password
       async beforeCreate(newUserData) {
-        // eslint-disable-next-line no-param-reassign
+        // hash the newUserData.password
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        // return the newUserData
         return newUserData;
       },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        // return the updatedUserData
+        return updatedUserData;
+      },
     },
+    // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
+    // don't pluralize name of database table
     freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
+    // make it so our model name stays lowercase in the database
     modelName: 'user',
   }
 );
