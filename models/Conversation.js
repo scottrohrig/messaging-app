960547d1d7156d21eec590189cf9f5c5862bd48f
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-02-10 21:11:51 -0800

    updated conversation models

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,36 +1,30 @@
 // require Model & DataTypes from sequelize
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 // require sequelize from connection.js
 const sequelize = require('../config/connection');
 // define the Conversation class that extends Model
 class Conversation extends Model {}
 // initialize the Conversation columns and options
 Conversation.init(
-    {
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    conversation_name{
-        type: DataTypes.STRING,
-        allowNull: false,
+    conversation_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-/*     user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-    }, */
-        sequelize,
-        timestamps: true,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'conversation'
-    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'conversation',
+  }
 );
 // modularize this script by exporting Conversation
 module.exports = Conversation;
--- models/index.js
+++ models/index.js
@@ -6,20 +6,25 @@ const Message = require('./Message');
 // setup User associations
 // user has many conversations
 User.hasMany(Conversation,{
-    foreignKey: 'user_id'
-    onDelete: "cascade"
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 // user has many messages
 User.hasMany(Message, {
     foreignKey:'user_id',
-    onDelete: "cascade"
+    onDelete: 'CASCADE'
 });
 // setup Conversation associations
 // conversation has many users
 Conversation.hasMany(User, {
-    foreignKey
+    foreignKey: 'user_id'
+    onDelete: 'CASCADE'
 })
 // conversation has many messages
+Conversation.hasMany(Message, {
+    foreignKey: 'user_id'
+    onDelete: 'CASCADE'
+})
 
 // setup message associations
 // message belongs to one conversation
