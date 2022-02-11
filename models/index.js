da2169a66f102ece07fb50d8f2116518f56704ca
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-02-11 09:13:37 -0800

    modified models/index.js

--- models/index.js
+++ models/index.js
@@ -5,30 +5,35 @@ const Message = require('./Message');
 
 // setup User associations
 // user has many conversations
-User.hasMany(Conversation,{
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+User.hasMany(Conversation, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 // user has many messages
 User.hasMany(Message, {
-    foreignKey:'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 // setup Conversation associations
 // conversation has many users
 Conversation.hasMany(User, {
-    foreignKey: 'user_id'
-    onDelete: 'CASCADE'
-})
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 // conversation has many messages
 Conversation.hasMany(Message, {
-    foreignKey: 'user_id'
-    onDelete: 'CASCADE'
-})
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
 // setup message associations
 // message belongs to one conversation
+Message.belongsTo(Conversation, {
+  foreignKey: 'user_id',
+});
 // message belongs to one user
-
+Message.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 // modularize this file by exporting an object containing each Model
-module.exports = { User };
+module.exports = { User, Conversation, Message };
