d6423efc93d2bd5818b97d3afd67db0c2f4f92f4
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-02-11 10:53:47 -0800

    modified models/User.js

--- models/User.js
+++ models/User.js
@@ -39,8 +39,8 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // this means the password must be at least eight characters long
-        len: [8],
+        // this means the password must be at least four characters long
+        len: [4],
       },
     },
   },
