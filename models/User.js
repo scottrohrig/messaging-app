a33da23acbd34a57fe8016c2df4a1a39fb9392b9
Mariko <mkaku98@gmail.com>
2022-02-11 23:17:54 -0800

    mari

--- models/User.js
+++ models/User.js
@@ -3,19 +3,47 @@ const { Model, DataTypes } = require('sequelize');
 // require sequelize from connection.js
 const sequelize = require('../config/connection');
 // require bcrypt
-const bcrypt = requier('bcrypt');
+const bcrypt = require('bcrypt');
 
 // define the User class that extends Model
 class User extends Model {
   // create an instance method that checks the password
   checkPassword(loginPw) {
     // return compared password from bcrypt. Consider making this asynchronous
+    return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
 // initialize the User Model's columns and options,
 User.init(
-  {},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
+  }
+}),
   {
     hooks: {
       // and add a beforeCreate method to the hooks to encrypt the user password
