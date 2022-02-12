3da0df1b72a58f39d1cb750209b065142bf12345
Scott Rohrig <scott.rohrig@gmail.com>
2022-02-12 12:38:24 -0800

    delete unneeded import

--- models/Participant.js
+++ models/Participant.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const { User, Conversation } = require('.');
 
 class Participant extends Model {}
 
