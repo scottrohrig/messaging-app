3872b45b37a9aaa7e36be0aab5f0f1749014cb97
Mariko <mkaku98@gmail.com>
2022-02-15 10:30:26 -0800

    db password typo fixed

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'mysql',
