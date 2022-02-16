d3257e1ab8aed5b243e326faceebcabb7ccdacd6
Darrell Markham <dnlmarkham@sbcglobal.net>
2022-02-16 10:12:49 -0800

    cleaned up

--- public/scripts/Authenticate.js
+++ public/scripts/Authenticate.js
@@ -4,11 +4,10 @@ const { User } = require('../../models');
 // get all users
 router.get('/', (req, res) => {
   User.findAll({
-    attributes: { exclude: ['password'] }
+    attributes: { exclude: ['password'] },
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
@@ -17,18 +16,17 @@ router.get('/:id', (req, res) => {
   User.findOne({
     attributes: { exclude: ['password'] },
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
-      console.log(err);
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
@@ -38,11 +36,10 @@ router.post('/', (req, res) => {
   User.create({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
@@ -51,9 +48,9 @@ router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
-      email: req.body.email
-    }
-  }).then(dbUserData => {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
     if (!dbUserData) {
       res.status(400).json({ message: 'No user with that email address!' });
       return;
@@ -77,18 +74,17 @@ router.put('/:id', (req, res) => {
   User.update(req.body, {
     individualHooks: true,
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData[0]) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
-      console.log(err);
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
@@ -96,18 +92,17 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
-      console.log(err);
+    .catch((err) => {
       res.status(500).json(err);
     });
 });
