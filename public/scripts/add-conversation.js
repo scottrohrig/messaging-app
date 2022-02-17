fd3f3ef7696f8ac768d347b73f97d54c8a6b0b80
scottrohrig <scott.rohrig@gmail.com>
2022-02-17 10:35:55 -0800

    handle new-conversation event

---
+++ public/scripts/add-conversation.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-alert */
+const handleCreateConversation = async (e) => {
+  e.preventDefault();
+
+  const conversationName = document
+    .getElementById('conversation-title')
+    .value.trim();
+  const recipient = document.getElementById('email-recipient').value.trim();
+
+  if (!conversationName || !recipient) {
+    return;
+  }
+  console.log('Fetching email...', conversationName, recipient);
+  const emailResponse = await fetch('/api/users', {
+    method: 'post',
+    body: JSON.stringify({
+      email: recipient,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  console.log('Validating email...');
+  if (!emailResponse.ok) {
+    alert('No user with that email!');
+    return;
+  }
+
+  console.log('Fetching Conversation...');
+  fetch('/api/conversations', {
+    method: 'post',
+    body: JSON.stringify({
+      conversation_name: conversationName,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((conversationResponse) => {
+    console.log('Validating Conversation...');
+    if (!conversationResponse.ok) {
+      console.log(conversationResponse.json());
+      alert(conversationResponse.statusText);
+    }
+  });
+};
+
+document
+  .getElementById('new-conversation-form')
+  .addEventListener('submit', handleCreateConversation);
