644cec014e8c61fe3166cb30365c8756d8d10f8b
scottrohrig <scott.rohrig@gmail.com>
2022-02-17 12:12:23 -0800

    in progress

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -1,1 +1,17 @@
-async function messageFormHandler(event) {}
+const { header } = require("express/lib/request");
+
+async function messageFormHandler(event) {
+  event.preventDefault();
+
+  const msgText = document.getElementById('new-message').value.trim()
+
+  const message = await fetch('/api/messages',{
+    method: 'post',
+    body: {
+      message_text: msgText,
+    }
+    headers:
+  })
+}
+
+document.getElementById('new-message').addEventListener('submit', messageFormHandler)
