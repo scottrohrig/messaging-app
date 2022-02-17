64c5d6b082d9f6866424752ad196d938f2bcd014
Mariko <mkaku98@gmail.com>
2022-02-16 21:11:21 -0800

    home routes login/signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -56,11 +56,14 @@ router.get('/login', (req, res) => {
     res.redirect('/');
     return;
   }
-
   res.render('login');
 });
 
 router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
   res.render('signup');
 });
 
