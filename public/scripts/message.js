6f94fb0ed37dfcbfff91f4596fd97df11ad68e0e
scottrohrig <scott.rohrig@gmail.com>
2022-02-17 12:29:43 -0800

    notes

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -1,17 +1,20 @@
-const { header } = require("express/lib/request");
-
+/* eslint-disable no-undef */
 async function messageFormHandler(event) {
   event.preventDefault();
 
-  const msgText = document.getElementById('new-message').value.trim()
+  const msgText = document.getElementById('new-message').value.trim();
 
-  const message = await fetch('/api/messages',{
+  const message = await fetch('/api/messages', {
     method: 'post',
     body: {
       message_text: msgText,
-    }
-    headers:
-  })
+      // TODO: [ ] - include logged in user_id
+      // TODO: [ ] - include current conversation_id (from url, eg, '.../conversation/1')
+    },
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
 
-document.getElementById('new-message').addEventListener('submit', messageFormHandler)
+document
+  .getElementById('new-message')
+  .addEventListener('submit', messageFormHandler);
