7f57ba15cd19f0bc7f8676d0971a7d67b99e8bf8
Darrell Markham <dnlmarkham@sbcglobal.net>
2022-02-17 17:36:47 -0800

    updated user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -136,17 +136,17 @@ router.put('/', async (req, res) => {
 router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
