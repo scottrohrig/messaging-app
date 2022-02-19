42f60396b50b36c13ec4ffbf5c72b5ed989b2b15
scottrohrig <scott.rohrig@gmail.com>
2022-02-19 09:34:15 -0700

    add delete functionality

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unreachable */
 const handleDeleteConversation = async (e) => {
   e.preventDefault();
 
+  console.log('delete');
+
   const id = e.target.closest('.delete-btn').dataset.cid;
+
   console.log('deleting conversation', id);
   const response = await fetch(`/api/conversations/${id}`, {
     method: 'DELETE',
@@ -11,10 +15,11 @@ const handleDeleteConversation = async (e) => {
   if (response.ok) {
     document.location.reload();
   } else {
+    // eslint-disable-next-line no-alert
     alert(response.statusText);
   }
 };
 
 document
-  .querySelector('#conversation-list')
+  .querySelector('[data-delete-btn]')
   .addEventListener('click', handleDeleteConversation);
