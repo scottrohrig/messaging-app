bde201de1b07d070a3f4d6024789c517cf8a72ea
scottrohrig <scott.rohrig@gmail.com>
2022-02-20 08:01:43 -0700

    add class method addParticipant

--- models/Conversation.js
+++ models/Conversation.js
@@ -3,7 +3,20 @@ const { Model, DataTypes } = require('sequelize');
 // require sequelize from connection.js
 const sequelize = require('../config/connection');
 // define the Conversation class that extends Model
-class Conversation extends Model {}
+class Conversation extends Model {
+  static addParticipant(body, models) {
+    // make a
+    return models.Participant.create({
+      user_id: body.user_id,
+      conversation_id: body.conversation_id,
+    }).then(() => {
+      return Conversation.findOne({
+        where: { id: body.conversation_id },
+        include: [{ model: models.Message }],
+      });
+    });
+  }
+}
 // initialize the Conversation columns and options
 Conversation.init(
   {
