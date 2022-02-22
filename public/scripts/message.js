e13af0e79b9c580d8445ad97e0098630b036f16d
Scott Rohrig <scott.rohrig@gmail.com>
2022-02-22 15:37:43 -0800

    update fetch request

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -1,16 +1,20 @@
+/* eslint-disable no-console */
 /* eslint-disable no-undef */
 async function messageFormHandler(event) {
   event.preventDefault();
 
+  const form = event.target;
+  const sendBtn = form.querySelector('.send-btn');
+
   const msgText = document.getElementById('new-message').value.trim();
-  const id = event.relatedTarget.getAttribute('data-cid');
+  const id = sendBtn.dataset.cid;
 
   const message = await fetch('/api/messages', {
     method: 'post',
-    body: {
+    body: JSON.stringify({
       message_text: msgText,
       conversation_id: id,
-    },
+    }),
     headers: { 'Content-Type': 'application/json' },
   });
 
@@ -20,26 +24,26 @@ async function messageFormHandler(event) {
 }
 
 document
-  .getElementById('message-form')
+  .querySelector('#messageForm')
   .addEventListener('submit', messageFormHandler);
 
-const addPeopleModal = document.getElementById('addPeopleModal');
-addPeopleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  const button = event.relatedTarget;
-  // Extract info from data-bs-* attributes
-  const id = button.getAttribute('data-bs-cid');
+// const addPeopleModal = document.getElementById('addPeopleModal');
+// addPeopleModal.addEventListener('show.bs.modal', function (event) {
+//   // Button that triggered the modal
+//   const button = event.relatedTarget;
+//   // Extract info from data-bs-* attributes
+//   const id = button.getAttribute('data-bs-cid');
 
-  const recipient = addPeopleModal
-    .querySelector('#add-person-email')
-    .value.trim();
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  const modalTitle = addPeopleModal.querySelector('.modal-title');
-  const modalBodyInput = addPeopleModal.querySelector('.modal-body input');
+//   const recipient = addPeopleModal
+//     .querySelector('#add-person-email')
+//     .value.trim();
+//   // If necessary, you could initiate an AJAX request here
+//   // and then do the updating in a callback.
+//   //
+//   // Update the modal's content.
+//   const modalTitle = addPeopleModal.querySelector('.modal-title');
+//   const modalBodyInput = addPeopleModal.querySelector('.modal-body input');
 
-  modalTitle.textContent = `New message to ${recipient}`;
-  modalBodyInput.value = recipient;
-});
+//   modalTitle.textContent = `New message to ${recipient}`;
+//   modalBodyInput.value = recipient;
+// });
