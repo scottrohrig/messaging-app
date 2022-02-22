ecb4d4c6c25d3c6d2c5540efb37d2c2d88233a08
Scott Rohrig <scott.rohrig@gmail.com>
2022-02-22 14:45:22 -0800

    order conversations by most recent message

--- controllers/api/conversation-routes.js
+++ controllers/api/conversation-routes.js
@@ -48,49 +48,52 @@ router.get('/:id', async (req, res) => {
 });
 
 // conversations by user
-router.get('/', async (req, res) => {
-  try {
-    const dbConversations = await Participant.findAll({
-      where: { user_id: 1 },
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-        {
-          model: Conversation,
-          attributes: ['conversation_name', 'updated_at'],
-          include: [
-            {
-              model: Message,
-              attributes: { exclude: ['created_at', 'conversation_id'] },
-              include: [
-                {
-                  model: User,
-                  attributes: ['username'],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-      // order by user_id decending
-      order: [
-        [{ model: Conversation }, 'updated_at', 'DESC'],
-        [{ model: Conversation }, { model: Message }, 'created_at', 'DESC'],
-      ],
-    });
+// router.get('/', async (req, res) => {
+//   if (!req.session) {
+//     return;
+//   }
+//   try {
+//     const dbConversations = await Participant.findAll({
+//       where: { user_id: req.session.user_id },
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['username'],
+//         },
+//         {
+//           model: Conversation,
+//           attributes: ['conversation_name', 'updated_at'],
+//           include: [
+//             {
+//               model: Message,
+//               attributes: { exclude: ['created_at', 'conversation_id'] },
+//               include: [
+//                 {
+//                   model: User,
+//                   attributes: ['username'],
+//                 },
+//               ],
+//             },
+//           ],
+//         },
+//       ],
+//       // order by user_id decending
+//       order: [
+//         [{ model: Conversation }, 'updated_at', 'DESC'],
+//         [{ model: Conversation }, { model: Message }, 'created_at', 'DESC'],
+//       ],
+//     });
 
-    // res.json(conversations);
-    const conversations = dbConversations.map((conversation) =>
-      conversation.get({ plain: true })
-    );
-    // console.log(conversations);
-    res.render('home', { conversations });
-  } catch (err) {
-    res.status(500).send(`<h1>ERROR: </h1><p>${err.message}</p>`);
-  }
-});
+//     // res.json(conversations);
+//     const conversations = dbConversations.map((conversation) =>
+//       conversation.get({ plain: true })
+//     );
+//     // console.log(conversations);
+//     res.render('home', { conversations });
+//   } catch (err) {
+//     res.status(500).send(`<h1>ERROR: </h1><p>${err.message}</p>`);
+//   }
+// });
 
 // CREATE new conversation
 router.post('/create', withAuth, async (req, res) => {
